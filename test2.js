5b905eb524122965c8bdf32c37e8c54d9819e760
serdkan <serdar.kandiran@outlook.com>
2024-07-17 22:47:00 +0300

    test9

---
+++ test2.js
@@ -0,0 +1,7 @@
+function paraustuhesapla(urunfiyati, paramiktari) {
+  return paramiktari - urunfiyati;// 150 tl 
+}
+
+let ortalama = dersnotuhesapla(50, 70);
+let ortalama2 = dersnotuhesapla(40, 80);
+let ortalama3 = dersnotuhesapla(60, 47);
